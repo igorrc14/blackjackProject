d03ee806b0e16557d79106587dda980eb9072ae0
igorrc14 <igor.rc@puccampinas.edu.br>
2024-12-13 10:29:53 -0300

    Implementação parcial do novo round

--- blackjack.cpp
+++ blackjack.cpp
@@ -21,7 +21,7 @@ void deckCreation(Card cards[52]) {
     }
 }
 
-void distributeCards(Card * card1, Card * card2, Card cards[52]) {
+void distributeInicialCards(Card * card1, Card * card2, Card cards[52], int inGameCards[16], int * count, int userCards[11], int * countUser) {
     srand(time(0));
     int randomNum1 = rand() % 52;
     int randomNum2 = rand() % 52;
@@ -31,15 +31,15 @@ void distributeCards(Card * card1, Card * card2, Card cards[52]) {
     }
     *card1 = cards[randomNum1];
     *card2 = cards[randomNum2];
-}
-
-void checkInicialAces(Card * card1, Card * card2, bool * firstIsAce, bool * secondIsAce) {
-    if (card1->face == "Ace") {
-        *firstIsAce = true;
-    }
-    if (card2->face == "Ace") {
-        *secondIsAce = true;
-    }
+    
+    inGameCards[*count] = randomNum1;
+    userCards[*countUser] = randomNum1;
+    (*count)++;
+    (*countUser)++;
+    inGameCards[*count] = randomNum2;
+    userCards[*countUser] = randomNum2;
+    (*count)++;
+    (*countUser)++;
 }
 
 bool checkWin(int * sum) {
@@ -50,14 +50,6 @@ bool checkWin(int * sum) {
     return false;
 }
 
-void calculateSum(int * sum, Card * card1, Card * card2) {
-    *sum = card1->value+card2->value;
-    if (*sum>21 && (card1->face == "Ace" || card2->face == "Ace")) {
-        *sum -= 10;
-    }
-    cout<<"TOTAL: "<<*sum<<endl;
-}
-
 void printCard (Card card) {
     if (card.face == "Ace") {
         cout<<card.face<<" of "<<card.suit<<" | Value: 1 or 11"<<endl;
@@ -66,26 +58,54 @@ void printCard (Card card) {
     cout<<card.face<<" of "<<card.suit<<" | Value: "<<card.value<<endl;
 }
 
+void calculateSum(int * sum, int userCards[11], Card cards[52], int * countUser) {
+    *sum = 0;
+    for (int i=0;i<*countUser; i++) {
+        *sum += cards[userCards[i]].value;
+    }
+}
+
+void hitNewCard(int * count, int inGameCards[16], Card cards[52],int userCards[11], int * countUser) {
+ // For some reason it suddently stopped working;
+ // TODO;
+}
+void newRound(int * count,int inGameCards[16], Card cards[52],int userCards[11], int * countUser, int * sum) {
+    int choice;
+    cout<<endl<<"1 - Hit"<<endl;
+    cout<<"2 - Stand"<<endl;
+    cout<<"Select: ";
+    cin>>choice;
+    switch (choice) {
+        case 1:
+            hitNewCard(count, inGameCards, cards, userCards, countUser);
+            calculateSum(sum, userCards,cards, count);
+        break;
+        case 2:
+            //dealer's turn;
+        break;
+    }
+}
+
 int main() {
-    bool firstIsAce = false;
-    bool secondIsAce = false;
-    int sum = 0, choice = 0;
+    int sum = 0, count = 0, countUser = 0, inGameCards[16], userCards[11];
     Card card1, card2, cards[52];
     deckCreation(cards);
     // Blackjack Game: Round 1
-    distributeCards(&card1,&card2,cards);
-
-    checkInicialAces(&card1,&card2,&firstIsAce,&secondIsAce);
+    distributeInicialCards(&card1,&card2,cards,inGameCards,&count,userCards, &countUser);
 
     printCard(card1);
     printCard(card2);
 
-    calculateSum(&sum, &card1, &card2);
+    calculateSum(&sum, userCards,cards, &count);
+    cout<<"TOTAL: "<<sum<<endl;
     if (checkWin(&sum)) {
         return 0;
     }
     // Blackjack Game: Round 2
-
-
+    newRound(&count, inGameCards, cards, userCards,&countUser, &sum);
+    cout<<"TOTAL: "<<sum<<endl;
+    if (checkWin(&sum)) {
+        return 0;
+    }
     return 0;
 }
